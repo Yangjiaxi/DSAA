dfc235b8570ad75ad5c2d8c3de28eb1698f3ba4c
Yangjiaxi <1468764755@qq.com>
2018-11-06 15:37:20 +0800

    <ADD> new some

---
+++ Chapter_01/Emoji/emoji.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int 😝(int 🤔)
+{
+    printf("%d\n", 🤔);
+    return 🤔 * 🤔;
+}
+
+int main()
+{
+    int 🐶 = 450;
+    int 🐂🍺 = 12;
+    printf("%d\n", 🐶);
+    printf("%d\n", 😝(🐂🍺));
+    return 0;
+}
--- Chapter_02/2.16_FastPow/main.c
+++ Chapter_02/2.16_FastPow/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int cnt = 0;
+
 long long FastPow(int a, int b) // compute a^b
 {
   long long res = 1;
@@ -9,10 +11,13 @@ long long FastPow(int a, int b) // compute a^b
     if (b % 2)
     {
       res *= base;
+      cnt++;
     }
     b >>= 1;
     base *= base;
+    cnt++;
   }
+  printf("Total : %d\n", cnt);
   return res;
 }
 
@@ -20,7 +25,7 @@ int main()
 {
   int a = -3;
   int b = 10;
-  printf("%d^%d = %lld", a, b, FastPow(a, b));
+  printf("%d^%d = %lld\n", a, b, FastPow(a, b));
   return 0;
 }
 
