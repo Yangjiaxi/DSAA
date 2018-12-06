e9d56e144a580134a9153b5d817f83b4500ac771
Yangjiaxi <1468764755@qq.com>
2018-12-06 21:41:06 +0800

    <MOD> format and rerun

--- Chapter_04/AvlTree/AvlTree.h
+++ Chapter_04/AvlTree/AvlTree.h
@@ -1,27 +1,27 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifndef AVLTREE_AVLTREE_H
 #define AVLTREE_AVLTREE_H
 
 struct AvlNode;
-typedef struct AvlNode *Position;
-typedef struct AvlNode *AvlTree;
+typedef struct AvlNode* Position;
+typedef struct AvlNode* AvlTree;
 
-AvlTree MakeEmpty(AvlTree T);                       //清空一棵树
-Position Find(int X, AvlTree T);                    //寻找值为X的节点并返回指向该节点的指针
+AvlTree MakeEmpty(AvlTree T);  //清空一棵树
+Position Find(int X, AvlTree T);  //寻找值为X的节点并返回指向该节点的指针
 Position FindMax(AvlTree T);                        //寻找最大的元素
 Position FindMin(AvlTree T);                        //寻找最小的元素
 AvlTree Insert(int X, AvlTree T);                   //插入元素X
 AvlTree Delete(int X, AvlTree T);                   //删除元素X
 static int Height(Position P);                      //返回节点P的高度
-static Position SingleRotateWithLeft(Position K2);  //insert LC->L，返回新的根
-static Position SingleRotateWithRight(Position K2); //insert RC->R，返回新的根
-static Position DoubleRotateWithLeft(Position K3);  //insert LC->R，返回新的根
-static Position DoubleRotateWithRight(Position K3); //insert RC->L，返回新的根
-static int Max(int a, int b);                       //比大小
-void InorderTreeWalk(AvlTree T);                    //中序遍历输出
-void PreTreeWalk(AvlTree T);                        //先序遍历输出
-void PostTreeWalk(AvlTree T);                       //后序遍历输出
+static Position SingleRotateWithLeft(Position K2);  // insert LC->L，返回新的根
+static Position SingleRotateWithRight(Position K2);  // insert RC->R，返回新的根
+static Position DoubleRotateWithLeft(Position K3);  // insert LC->R，返回新的根
+static Position DoubleRotateWithRight(Position K3);  // insert RC->L，返回新的根
+static int Max(int a, int b);                        //比大小
+void InorderTreeWalk(AvlTree T);                     //中序遍历输出
+void PreTreeWalk(AvlTree T);                         //先序遍历输出
+void PostTreeWalk(AvlTree T);                        //后序遍历输出
 
-#endif //AVLTREE_AVLTREE_H
+#endif  // AVLTREE_AVLTREE_H
--- Chapter_04/AvlTree/main.c
+++ Chapter_04/AvlTree/main.c
@@ -3,39 +3,50 @@
 
 int main()
 {
-  AvlTree AT = NULL;
-  MakeEmpty(AT);
-  AT = Insert(11, AT);
-  AT = Insert(9, AT);
-  AT = Insert(12, AT);
-  AT = Insert(8, AT);
-  AT = Insert(10, AT);
+    AvlTree AT = NULL;
+    AT = MakeEmpty(AT);
+    // for (int i = 9; i >= 1; i--)
+    // {
+    //     AT = Insert(i, AT);
+    // }
+    // 9, 7, 8, 4, 6, 5, 3, 1, 2
+    AT = Insert(18, AT);
+    AT = Insert(4, AT);
+    AT = Insert(12, AT);
+    AT = Insert(13, AT);
+    AT = Insert(7, AT);
+    AT = Insert(27, AT);
+    AT = Insert(83, AT);
+    AT = Insert(38, AT);
+    AT = Insert(24, AT);
+    AT = Insert(41, AT);
 
-  printf("\nInorder:");
-  InorderTreeWalk(AT);
-  printf("\nPreOrder:");
-  PreTreeWalk(AT);
-  printf("\nPostOrder:");
-  PostTreeWalk(AT);
+    printf("\nInorder:");
+    InorderTreeWalk(AT);
+    printf("\nPreOrder:");
+    PreTreeWalk(AT);
+    printf("\nPostOrder:");
+    PostTreeWalk(AT);
 
-  printf("\n\nDel Ele [12]\n");
-  AT = Delete(12, AT);
-  printf("\nInorder:");
-  InorderTreeWalk(AT);
-  printf("\nPreOrder:");
-  PreTreeWalk(AT);
-  printf("\nPostOrder:");
-  PostTreeWalk(AT);
+    // printf("\n\nDel Ele [12]\n");
+    // AT = Delete(12, AT);
+    // printf("\nInorder:");
+    // InorderTreeWalk(AT);
+    // printf("\nPreOrder:");
+    // PreTreeWalk(AT);
+    // printf("\nPostOrder:");
+    // PostTreeWalk(AT);
 
-  printf("\n\nIns Ele [7]\n");
-  AT = Insert(7, AT);
-  printf("\nInorder:");
-  InorderTreeWalk(AT);
-  printf("\nPreOrder:");
-  PreTreeWalk(AT);
-  printf("\nPostOrder:");
-  PostTreeWalk(AT);
+    // printf("\n\nIns Ele [7]\n");
+    // AT = Insert(7, AT);
+    // printf("\nInorder:");
+    // InorderTreeWalk(AT);
+    // printf("\nPreOrder:");
+    // PreTreeWalk(AT);
+    // printf("\nPostOrder:");
+    // PostTreeWalk(AT);
 
-  MakeEmpty(AT);
-  return 0;
+    MakeEmpty(AT);
+    printf("\n");
+    return 0;
 }
